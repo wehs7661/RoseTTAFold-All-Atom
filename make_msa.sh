a68356e99597291a0b4139a908d4a8ce889f93ed
Wei-Tse Tsu <bioc1870@scn02.maas>
2024-11-08 01:41:55 +0000

    Modified base.yaml, make_msa.sh and preprocessing.py

--- make_msa.sh
+++ make_msa.sh
@@ -11,6 +11,9 @@ MEM="$4"
 # template database
 DB_TEMPL="$5"
 
+# MSA path to check
+MSA_PATH="$6"
+
 # current script directory (i.e., pipe directory)
 SCRIPT=`realpath -s $0`
 export PIPE_DIR=`dirname $SCRIPT`
@@ -39,87 +42,99 @@ mkdir -p $out_dir/hhblits
 tmp_dir="$out_dir/hhblits"
 out_prefix="$out_dir/t000_"
 
-# perform iterative searches against UniRef30
-if [ ! -s ${out_prefix}.msa0.a3m ]
+# Check if an MSA file is provided. If so, skip the MSA generation step and use the provided file.
+if [ -n "$MSA_PATH" ] && [ -s "$MSA_PATH" ]
 then
-    prev_a3m="$trim_fasta"
-    for e in 1e-10 1e-6 1e-3
-    do
-        echo "Running HHblits against UniRef30 with E-value cutoff $e"
-        if [ ! -s $tmp_dir/t000_.$e.a3m ]
-        then
-            $HHBLITS_UR30 -i $prev_a3m -oa3m $tmp_dir/t000_.$e.a3m -e $e -v 0
-        fi
-        hhfilter -maxseq 100000 -id 90 -cov 75 -i $tmp_dir/t000_.$e.a3m -o $tmp_dir/t000_.$e.id90cov75.a3m
-        hhfilter -maxseq 100000 -id 90 -cov 50 -i $tmp_dir/t000_.$e.a3m -o $tmp_dir/t000_.$e.id90cov50.a3m
-        prev_a3m="$tmp_dir/t000_.$e.id90cov50.a3m"
-        n75=`grep -c "^>" $tmp_dir/t000_.$e.id90cov75.a3m`
-        n50=`grep -c "^>" $tmp_dir/t000_.$e.id90cov50.a3m`
-
-        if ((n75>2000))
-        then
-            if [ ! -s ${out_prefix}.msa0.a3m ]
-            then
-                cp $tmp_dir/t000_.$e.id90cov75.a3m ${out_prefix}.msa0.a3m
-                break
-            fi
-        elif ((n50>4000))
-        then
-            if [ ! -s ${out_prefix}.msa0.a3m ]
-            then
-                cp $tmp_dir/t000_.$e.id90cov50.a3m ${out_prefix}.msa0.a3m
-                break
-            fi
-        else
-            continue
-        fi
-    done
-
-    # perform iterative searches against BFD if it failes to get enough sequences
-    if [ ! -s ${out_prefix}.msa0.a3m ] 
-    then
-        e=1e-3
-        echo "Running HHblits against BFD with E-value cutoff $e"
-        if [ ! -s $tmp_dir/t000_.$e.bfd.a3m ]
-        then
-            $HHBLITS_BFD -i $prev_a3m -oa3m $tmp_dir/t000_.$e.bfd.a3m -e $e -v 0
-        fi
-        hhfilter -maxseq 100000 -id 90 -cov 75 -i $tmp_dir/t000_.$e.bfd.a3m -o $tmp_dir/t000_.$e.bfd.id90cov75.a3m
-        hhfilter -maxseq 100000 -id 90 -cov 50 -i $tmp_dir/t000_.$e.bfd.a3m -o $tmp_dir/t000_.$e.bfd.id90cov50.a3m
-        prev_a3m="$tmp_dir/t000_.$e.bfd.id90cov50.a3m"
-        n75=`grep -c "^>" $tmp_dir/t000_.$e.bfd.id90cov75.a3m`
-        n50=`grep -c "^>" $tmp_dir/t000_.$e.bfd.id90cov50.a3m`
-
-        if ((n75>2000))
-        then
-            if [ ! -s ${out_prefix}.msa0.a3m ]
-            then
-                cp $tmp_dir/t000_.$e.bfd.id90cov75.a3m ${out_prefix}.msa0.a3m
-            fi
-        elif ((n50>4000))
-        then
-            if [ ! -s ${out_prefix}.msa0.a3m ]
-            then
-                cp $tmp_dir/t000_.$e.bfd.id90cov50.a3m ${out_prefix}.msa0.a3m
-            fi
-        fi
-    fi
-
+    echo "Using the provided MSA file: $MSA_PATH"
+    MSA_TO_USE=$MSA_PATH
+else
+    # perform iterative searches against UniRef30
     if [ ! -s ${out_prefix}.msa0.a3m ]
     then
-        cp $prev_a3m ${out_prefix}.msa0.a3m
+        prev_a3m="$trim_fasta"
+        for e in 1e-10 1e-6 1e-3
+        do
+            echo "Running HHblits against UniRef30 with E-value cutoff $e"
+            if [ ! -s $tmp_dir/t000_.$e.a3m ]
+            then
+                $HHBLITS_UR30 -i $prev_a3m -oa3m $tmp_dir/t000_.$e.a3m -e $e -v 0
+            fi
+            hhfilter -maxseq 100000 -id 90 -cov 75 -i $tmp_dir/t000_.$e.a3m -o $tmp_dir/t000_.$e.id90cov75.a3m
+            hhfilter -maxseq 100000 -id 90 -cov 50 -i $tmp_dir/t000_.$e.a3m -o $tmp_dir/t000_.$e.id90cov50.a3m
+            prev_a3m="$tmp_dir/t000_.$e.id90cov50.a3m"
+            n75=`grep -c "^>" $tmp_dir/t000_.$e.id90cov75.a3m`
+            n50=`grep -c "^>" $tmp_dir/t000_.$e.id90cov50.a3m`
+
+            if ((n75>2000))
+            then
+                if [ ! -s ${out_prefix}.msa0.a3m ]
+                then
+                    cp $tmp_dir/t000_.$e.id90cov75.a3m ${out_prefix}.msa0.a3m
+                    break
+                fi
+            elif ((n50>4000))
+            then
+                if [ ! -s ${out_prefix}.msa0.a3m ]
+                then
+                    cp $tmp_dir/t000_.$e.id90cov50.a3m ${out_prefix}.msa0.a3m
+                    break
+                fi
+            else
+                continue
+            fi
+        done
+
+        # perform iterative searches against BFD if it failes to get enough sequences
+        if [ ! -s ${out_prefix}.msa0.a3m ] 
+        then
+            e=1e-3
+            echo "Running HHblits against BFD with E-value cutoff $e"
+            if [ ! -s $tmp_dir/t000_.$e.bfd.a3m ]
+            then
+                $HHBLITS_BFD -i $prev_a3m -oa3m $tmp_dir/t000_.$e.bfd.a3m -e $e -v 0
+            fi
+            hhfilter -maxseq 100000 -id 90 -cov 75 -i $tmp_dir/t000_.$e.bfd.a3m -o $tmp_dir/t000_.$e.bfd.id90cov75.a3m
+            hhfilter -maxseq 100000 -id 90 -cov 50 -i $tmp_dir/t000_.$e.bfd.a3m -o $tmp_dir/t000_.$e.bfd.id90cov50.a3m
+            prev_a3m="$tmp_dir/t000_.$e.bfd.id90cov50.a3m"
+            n75=`grep -c "^>" $tmp_dir/t000_.$e.bfd.id90cov75.a3m`
+            n50=`grep -c "^>" $tmp_dir/t000_.$e.bfd.id90cov50.a3m`
+
+            if ((n75>2000))
+            then
+                if [ ! -s ${out_prefix}.msa0.a3m ]
+                then
+                    cp $tmp_dir/t000_.$e.bfd.id90cov75.a3m ${out_prefix}.msa0.a3m
+                fi
+            elif ((n50>4000))
+            then
+                if [ ! -s ${out_prefix}.msa0.a3m ]
+                then
+                    cp $tmp_dir/t000_.$e.bfd.id90cov50.a3m ${out_prefix}.msa0.a3m
+                fi
+            fi
+        fi
+
+        if [ ! -s ${out_prefix}.msa0.a3m ]
+        then
+            cp $prev_a3m ${out_prefix}.msa0.a3m
+        fi
     fi
+
+    MSA_TO_USE=${out_prefix}.msa0.a3m
+
 fi
-    
+
 echo "Running PSIPRED"
+echo "  MSA file used: $MSA_TO_USE"
 mkdir -p $out_dir/log
-$PIPE_DIR/input_prep/make_ss.sh $out_dir/t000_.msa0.a3m $out_dir/t000_.ss2 > $out_dir/log/make_ss.stdout 2> $out_dir/log/make_ss.stderr
+$PIPE_DIR/input_prep/make_ss.sh $MSA_TO_USE $out_dir/t000_.ss2 > $out_dir/log/make_ss.stdout 2> $out_dir/log/make_ss.stderr
 
 if [ ! -s $out_dir/t000_.hhr ]
 then
     echo "Running hhsearch"
+    echo "  MSA file used: $MSA_TO_USE"
     HH="hhsearch -b 50 -B 500 -z 50 -Z 500 -mact 0.05 -cpu $CPU -maxmem $MEM -aliw 100000 -e 100 -p 5.0 -d $DB_TEMPL"
     
-    cat $out_dir/t000_.ss2 $out_dir/t000_.msa0.a3m > $out_dir/t000_.msa0.ss2.a3m
+    cat $out_dir/t000_.ss2 $MSA_TO_USE > $out_dir/t000_.msa0.ss2.a3m
     $HH -i $out_dir/t000_.msa0.ss2.a3m -o $out_dir/t000_.hhr -atab $out_dir/t000_.atab -v 0
 fi

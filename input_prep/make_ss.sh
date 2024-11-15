73c612b0fd1f84d93d98d38a31e7eda3a1b69b3f
wehs7661 <wehs7661@colorado.edu>
2024-11-15 01:31:26 +0000

    Modified make_ss.sh for enable RFAA parallelism without FileNotFoundError

--- input_prep/make_ss.sh
+++ input_prep/make_ss.sh
@@ -9,24 +9,29 @@ echo $DATADIR
 i_a3m="$1"
 o_ss="$2"
 
-ID=$(basename $i_a3m .a3m).tmp
+output_dir=$(dirname "$i_a3m")  # we same the tmp files in the same directory as the input a3m file
+ID=tmp  # Here we make it simpler than the original script
 
-$PIPE_DIR/csblast-2.2.3/bin/csbuild -i $i_a3m -I a3m -D $PIPE_DIR/csblast-2.2.3/data/K4000.crf -o $ID.chk -O chk
+$PIPE_DIR/csblast-2.2.3/bin/csbuild -i $i_a3m -I a3m -D $PIPE_DIR/csblast-2.2.3/data/K4000.crf -o $output_dir/$ID.chk -O chk
 
-head -n 2 $i_a3m > $ID.fasta
-echo $ID.chk > $ID.pn
-echo $ID.fasta > $ID.sn
+head -n 2 $i_a3m > $output_dir/$ID.fasta
+echo $output_dir/$ID.chk > $output_dir/$ID.pn
+echo $output_dir/$ID.fasta > $output_dir/$ID.sn
 
+# makemat seems to expect relative path so it's easier to cd to the output directory and then go back to the original directory
+cd $output_dir
 makemat -P $ID
-psipred $ID.mtx $DATADIR/weights.dat $DATADIR/weights.dat2 $DATADIR/weights.dat3 > $ID.ss
-psipass2 $DATADIR/weights_p2.dat 1 1.0 1.0 $i_a3m.csb.hhblits.ss2 $ID.ss > $ID.horiz
+cd -
+
+psipred $output_dir/$ID.mtx $DATADIR/weights.dat $DATADIR/weights.dat2 $DATADIR/weights.dat3 > $output_dir/$ID.ss
+psipass2 $DATADIR/weights_p2.dat 1 1.0 1.0 $i_a3m.csb.hhblits.ss2 $output_dir/$ID.ss > $output_dir/$ID.horiz
 
 (
 echo ">ss_pred"
-grep "^Pred" $ID.horiz | awk '{print $2}'
+grep "^Pred" $output_dir/$ID.horiz | awk '{print $2}'
 echo ">ss_conf"
-grep "^Conf" $ID.horiz | awk '{print $2}'
+grep "^Conf" $output_dir/$ID.horiz | awk '{print $2}'
 ) | awk '{if(substr($1,1,1)==">") {print "\n"$1} else {printf "%s", $1}} END {print ""}' | sed "1d" > $o_ss
 
 rm ${i_a3m}.csb.hhblits.ss2
-rm $ID.*
+rm $output_dir/$ID.*
